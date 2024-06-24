c624930b27fa84b2a7892b8b7ea6287b2255a338
osugisan <es1000648548@gmail.com>
2024-06-24 11:45:58 +0900

    マイページ

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Reservation;
 use App\Models\User;
 use App\Services\MypageService;
+use Carbon\CarbonImmutable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,5 +21,30 @@ class MypageController extends Controller
         $pastEvents = MypageService::reservedEvent($events, 'past');
 
         return view('mypage/index', compact('fromTodayEvents', 'pastEvents'));
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        $reservation = Reservation::where('user_id', Auth::id())
+            ->where('event_id', $id)
+            ->latest()
+            ->first();
+
+        return view('mypage/show', compact('event', 'reservation'));
+    }
+
+    public function cancel($id)
+    {
+        $reservation = Reservation::where('user_id', Auth::id())
+            ->where('event_id', $id)
+            ->latest()
+            ->first();
+
+        $reservation->canceled_date = CarbonImmutable::now()->format('Y-m-d H:i:s');
+        $reservation->save();
+
+        session()->flash('status', 'キャンセルOK!!');
+        return to_route('dashboard');
     }
 }
